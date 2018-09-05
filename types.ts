18f2144d0b2e06643794c170440b56e4fb7a390d
Josh Smeaton <josh.smeaton@gmail.com>
2018-09-05 16:35:25 +1000

    Remove unused types file

--- types.ts
+++
@@ -1,8 +0,0 @@
-interface ChallengeRequest {
-    token: string,
-    challenge: string,
-    type: "url_verification"
-}
-
-type CallbackError = string | null
-type LambdaCallback = (error: CallbackError, response: Object) => void
